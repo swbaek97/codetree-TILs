c48ce6e5a2d2e35b0e5ed380ac8043fd8e8ff3e1
swbaek97 <80388349+swbaek97@users.noreply.github.com>
2024-01-23 11:30:50 +0900

    Title: 좀 더 어려운 수학 점수, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240123/좀 더 어려운 수학 점수/math-scores-are-more-difficult.py
@@ -0,0 +1,12 @@
+m1, e1 = map(int, input().split())
+m2, e2 = map(int, input().split())
+
+if m1 > m2:
+    print('A')
+elif m1 < m2:
+    print('B')
+else:
+    if e1 > e2:
+        print('A')
+    else:
+        print('B')
