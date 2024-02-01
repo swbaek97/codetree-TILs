b0b735696723338d855b04dacba80ab8203b695a
swbaek97 <80388349+swbaek97@users.noreply.github.com>
2024-02-01 16:59:59 +0900

    Title: 좌우 지그재그 출력, Time: 187ms, Memory: 26MB, Status: Passed - CodeTree

--- 240201/좌우 지그재그 출력/print-lr-zigzag.py
+++ 240201/좌우 지그재그 출력/print-lr-zigzag.py
@@ -1,17 +1,9 @@
 n = int(input())
 
-cnt = 1
-
 for i in range(n):
     for j in range(n):
         if i % 2 == 0:
-            print(cnt, end=' ')
-            cnt += 1
+            print((n*i) + j + 1, end=' ')
         else:
-            if j == 0:
-                cnt += n-1
-            print(cnt, end=' ')
-            cnt -= 1
-            if j == n-1:
-                cnt += n+1
+            print((n*i) + n - j, end=' ')
     print()
